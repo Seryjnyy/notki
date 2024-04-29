2e1efcfb52f6a87122ebb86b4f6817cfbbd1780a
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-04-29 23:59:29 +0100

    Improvements

--- src/components/ui/note-card.tsx
+++ src/components/ui/note-card.tsx
@@ -1,31 +1,118 @@
-import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card";
-import { cn } from "@/lib/utils";
-import { ReactNode } from "react";
+import { ScrollArea, ScrollBar } from "@/components/ui/scroll-area";
+import {
+  Tooltip,
+  TooltipContent,
+  TooltipProvider,
+  TooltipTrigger,
+} from "@/components/ui/tooltip";
+
+import {
+  Card,
+  CardContent,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from "@/components/ui/card";
+import { Note, NoteSettings } from "@/lib/types";
+import { cn, formatBytes, unixToTimestamp } from "@/lib/utils";
+import { CopyIcon, Cross1Icon } from "@radix-ui/react-icons";
+import { useMemo } from "react";
+import { Button } from "./button";
 
 export default function NoteCard({
-  children,
-  title,
   seeTitles,
+  note,
+  settings,
+  handleDelete,
 }: {
-  children: ReactNode;
-  title: string;
   seeTitles: boolean;
+  note: Note;
+  settings: NoteSettings;
+  handleDelete: (id: string) => void;
 }) {
-  console.log(seeTitles);
+  const formattedDate = useMemo(() => {
+    return unixToTimestamp(note.lastModified);
+  }, [note]);
+
+  const formattedBytes = useMemo(() => formatBytes(note.size), [note]);
+
+  const onDelete = () => {
+    handleDelete(note.id);
+  };
+
+  // basis-full, 1/4
   return (
-    <Card>
-      <CardHeader>
+    <Card className="basis-full">
+      <CardHeader className="flex flex-row items-center justify-between">
         <CardTitle
           className={cn("text-muted-foreground select-none", {
             "sr-only": !seeTitles,
           })}
         >
-          {title}
+          <div className="w-[16rem] overflow-x-clip">{note.fileName}</div>
         </CardTitle>
+        {settings.actions.visible && (
+          <div className="w-fit ml-auto space-x-8">
+            {settings.actions.options.copy && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button className="px-2 py-0" variant={"ghost"}>
+                      <CopyIcon />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Copy contents</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+
+            {settings.actions.options.remove && (
+              <TooltipProvider>
+                <Tooltip>
+                  <TooltipTrigger asChild>
+                    <Button
+                      className="px-2 py-0"
+                      variant={"ghost"}
+                      onClick={onDelete}
+                    >
+                      <Cross1Icon />
+                    </Button>
+                  </TooltipTrigger>
+                  <TooltipContent>
+                    <p>Remove note</p>
+                  </TooltipContent>
+                </Tooltip>
+              </TooltipProvider>
+            )}
+          </div>
+        )}
       </CardHeader>
-      <CardContent className="leading-normal">
-        <pre>{children}</pre>
+      <CardContent className={`leading-${settings.lineHeight}`}>
+        <ScrollArea>
+          <pre className="pb-6">{note.content}</pre>
+          <ScrollBar orientation="horizontal" />
+        </ScrollArea>
       </CardContent>
+      {settings.metadata.visible && (
+        <CardFooter className="flex justify-between flex-wrap gap-4 select-none">
+          <div className="space-x-4">
+            <span className="text-sm text-muted-foreground">
+              {settings.metadata.options.lastModified && formattedDate}
+            </span>
+            <span className="text-sm text-muted-foreground">
+              {settings.metadata.options.size && formattedBytes}
+            </span>
+          </div>
+          <div className="space-x-4">
+            <span className="text-sm text-muted-foreground">
+              {settings.metadata.options.characterCount &&
+                `${note.characterCount} characters`}
+            </span>
+          </div>
+        </CardFooter>
+      )}
     </Card>
   );
 }
---
+++ src/services/preferences.ts
@@ -0,0 +1,39 @@
+import { NoteSettings } from "@/lib/types";
+
+const PREFERENCE_STORE = "txt-viewer-preferences";
+
+export function getDefaultPreferences(): NoteSettings {
+  const preferences: NoteSettings = {
+    titles: true,
+    lineHeight: "normal",
+    metadata: {
+      visible: false,
+      options: { size: true, lastModified: true, characterCount: true },
+    },
+    actions: { visible: true, options: { remove: true, copy: false } },
+  };
+  savePreferences(preferences);
+
+  return preferences;
+}
+
+export function removePreferences() {
+  localStorage.removeItem(PREFERENCE_STORE);
+}
+
+export function savePreferences(preferences: NoteSettings) {
+  localStorage.setItem(PREFERENCE_STORE, JSON.stringify(preferences));
+}
+
+export function getPreferences() {
+  const store = localStorage.getItem(PREFERENCE_STORE);
+
+  if (!store) {
+    return getDefaultPreferences();
+  }
+
+  // WARN : BLINDLY TRUSTING HERE
+  const parsed: NoteSettings = JSON.parse(store);
+
+  return parsed;
+}
