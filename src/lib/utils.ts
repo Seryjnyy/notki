4c2cbefda7a92c51038e74978ba793b54799fa70
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-05-01 17:27:28 +0100

    Added more customisation options
    - Enabled user to change padding of content.
    
    - Enabled user to change font size.
    
    - Enabled user to change note list column amount.
    
    - Added Immer for easier and cleaner state updates of settings.

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -61,7 +61,7 @@ export function formatBytes(bytes: number) {
 
 // https://stackoverflow.com/a/6860916
 export function guidGenerator() {
-  var S4 = function () {
+  const S4 = function () {
     return (((1 + Math.random()) * 0x10000) | 0).toString(16).substring(1);
   };
   return "k" + S4() + S4() + "-" + S4() + "-" + S4();
--- src/services/preferences.ts
+++ src/services/preferences.ts
@@ -4,19 +4,49 @@ const PREFERENCE_STORE = "txt-viewer-preferences";
 
 export function getDefaultPreferences(): NoteSettings {
   const preferences: NoteSettings = {
-    titles: true,
     metadata: {
       visible: false,
       options: { size: true, lastModified: true, characterCount: true },
     },
-    actions: { visible: true, options: { remove: true, copy: false } },
+    header: {
+      visible: true,
+      options: {
+        title: true,
+        actions: { visible: true, options: { copy: false, remove: true } },
+      },
+    },
     sort: { sortBy: "none", order: "asc" },
-    content: { letterSpacing: "normal", lineHeight: "normal" },
+    content: {
+      letterSpacing: getDefaultLetterSpacing(),
+      lineHeight: getDefaultLineHeight(),
+    },
+    styling: {
+      note: {
+        paddingX: getDefaultPaddingX(),
+        paddingTop: getDefaultPaddingTop(),
+        paddingBottom: getDefaultPaddingBottom(),
+      },
+      list: {
+        columns: getDefaultColumns(),
+      },
+      content: {
+        fontSize: getDefaultFontSize(),
+      },
+    },
   };
   savePreferences(preferences);
 
   return preferences;
 }
+
+// Why functions?
+export const getDefaultLetterSpacing = () => 0;
+export const getDefaultLineHeight = () => 1.25;
+export const getDefaultPaddingX = () => 1;
+export const getDefaultPaddingTop = () => 0;
+export const getDefaultPaddingBottom = () => 1.5;
+export const getDefaultColumns = () => 1;
+export const getDefaultFontSize = () => 1;
 
 export function removePreferences() {
   localStorage.removeItem(PREFERENCE_STORE);
