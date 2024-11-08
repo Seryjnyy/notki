55da558a2f3094c4dc1cb4d8cc4e8f56b50e2b6b
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-08 12:45:22 +0000

    Add new font system
    - Added a new system based on
      https://github.com/imsandeshpandey/typelabs that was inspired by
      https://monkeytype.com/.

---
+++ apps/www-test/src/config/local-storage-keys.config.ts
@@ -0,0 +1,7 @@
+export const CURRENT_STYLE_KEY = "color_style";
+
+export const BORDER_RADIUS_KEY = "border_radius";
+
+export const CURRENT_FONT_KEY = "current_font";
+export const USER_FONTS_KEY = "user_fonts";
+export const FONT_SIZE_KEY = "font_size";
