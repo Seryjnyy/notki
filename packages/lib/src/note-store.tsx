1568a126be43d6e26fc71dfa937c4a084ae371e4
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-11 14:54:37 +0000

    Add selector to note store
    - Oops forgot to commit this before.

--- packages/lib/src/note-store.tsx
+++ packages/lib/src/note-store.tsx
@@ -1,16 +1,19 @@
 import { create } from "zustand";
 import { Note } from "./types";
+import { createSelectors } from "./create-zustand-selectors";
 
 interface NotesState {
-  notes: Note[];
-  setNotes: (newNotes: Note[]) => void;
-  // addNote: (newNote: Note) => void;
+    notes: Note[];
+    setNotes: (newNotes: Note[]) => void;
+    // addNote: (newNote: Note) => void;
 }
 
-const useNoteStore = create<NotesState>()((set) => ({
-  notes: [],
-  setNotes: (newNotes) => set(() => ({ notes: newNotes })),
-  // addNote: (newNote) => set((state) => ({ notes: [...state.notes, newNote] })),
+const useNoteStoreBase = create<NotesState>()((set) => ({
+    notes: [],
+    setNotes: (newNotes) => set(() => ({ notes: newNotes })),
+    // addNote: (newNote) => set((state) => ({ notes: [...state.notes, newNote] })),
 }));
+
+const useNoteStore = createSelectors(useNoteStoreBase);
 
 export { useNoteStore };
