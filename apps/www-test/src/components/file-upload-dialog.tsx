79b81936e9904e2130fddabb8ba8330737aa4e22
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-14 23:33:26 +0000

    Fix file upload dialog locking the note nav
    - After uploading something, the dialog is closed through local state
      and it doesn't go through the navigation-aware-dialog onOpenChange
      function. So nav does not get enabled. Fixed it by calling it
      manually.

--- apps/www-test/src/components/file-upload-dialog.tsx
+++ apps/www-test/src/components/file-upload-dialog.tsx
@@ -6,24 +6,29 @@ import {
     DialogTitle,
     DialogTrigger,
 } from "@repo/ui/components/ui/dialog";
-import DropZone from "@repo/ui/components/ui/sections/drop-zone";
 import { Label } from "@repo/ui/components/ui/label";
 import { RadioGroup, RadioGroupItem } from "@repo/ui/components/ui/radio-group";
+import DropZone from "@repo/ui/components/ui/sections/drop-zone";
 import { Upload } from "lucide-react";
-import { NavigationAwareDialog } from "./compound-ui/navigation-aware-components";
 import { useState } from "react";
+import { useNavigationLock } from "~/hooks/use-navigation-lock";
+import { NavigationAwareDialog } from "./compound-ui/navigation-aware-components";
 
 export default function FileUploadDialog() {
     const [open, setOpen] = useState(false);
+    const { enableNavigation } = useNavigationLock();
+
+    const onUpload = () => {
+        // When dialog is closed manually it needs to update navigation lock
+        enableNavigation();
+        setOpen(false);
+        window.scrollTo(0, 0);
+    };
 
     return (
         <NavigationAwareDialog open={open} onOpenChange={setOpen}>
             <DialogTrigger asChild>
-                <Button
-                    size={"icon"}
-                    variant={"secondary"}
-                    onClick={() => window.scrollTo(0, 0)}
-                >
+                <Button size={"icon"} variant={"secondary"}>
                     <Upload className="text-primary size-4" />
                 </Button>
             </DialogTrigger>
@@ -35,7 +40,7 @@ export default function FileUploadDialog() {
                         replace the existing notes.
                     </DialogDescription>
                 </DialogHeader>
-                <DropZone onSuccess={() => setOpen(false)} />
+                <DropZone onSuccess={onUpload} />
                 <RadioGroup defaultValue="option-one" className="flex">
                     <div className="flex items-center space-x-2">
                         <RadioGroupItem value="option-one" id="option-one" />
