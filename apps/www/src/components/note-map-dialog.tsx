60cf796a8232e1de8876e69098251d734c3dee1a
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-16 23:10:45 +0000

    Add controls to the note-map-dialog
    - Added slider and buttons to control zoom.
    
    - Added reset buttons for position and zoom
    
    - Added toolbar.

--- apps/www/src/components/note-map-dialog.tsx
+++ apps/www/src/components/note-map-dialog.tsx
@@ -17,6 +17,7 @@ import { Map, Minus, Plus, Search, Undo2 } from "lucide-react";
 import React, { memo, useEffect, useMemo, useRef, useState } from "react";
 
 import { useFilteredNotes } from "@repo/lib/stores/note-store";
+import { Slider } from "@repo/ui/components/ui/slider";
 import { useNotes } from "~/hooks/use-notes";
 import { NavigationAwareDialog } from "./compound-ui/navigation-aware-components";
 import NoteCard from "./compound-ui/note-card";
@@ -37,6 +38,51 @@ const Notes = memo(() => {
 
     return <ul>{memoedNotes}</ul>;
 });
+
+const Controls = ({
+    scale,
+    setScale,
+    scaleLimits,
+}: {
+    scale: number;
+    setScale: (scale: number) => void;
+    scaleLimits: { min: number; max: number };
+}) => {
+    const step = 0.1;
+
+    return (
+        <div className="max-w-[10rem] min-w-[10rem] flex items-center gap-1 border px-2 py-1 rounded-[var(--radius)]">
+            <Search />
+            <Button
+                size={"icon"}
+                variant={"ghost"}
+                onClick={() => {
+                    if (scale - step <= scaleLimits.min) return;
+                    setScale(scale - step);
+                }}
+            >
+                <Minus className="size-3" />
+            </Button>
+            <Slider
+                max={scaleLimits.max}
+                min={scaleLimits.min}
+                step={step}
+                value={[scale]}
+                onValueChange={(val) => setScale(val[0])}
+            />
+            <Button
+                size={"icon"}
+                variant={"ghost"}
+                onClick={() => {
+                    if (scale + step > scaleLimits.max) return;
+                    setScale(scale + step);
+                }}
+            >
+                <Plus className="size-3" />
+            </Button>
+        </div>
+    );
+};
 
 const NoteMap = () => {
     const [scale, setScale] = useState(1);
@@ -65,10 +111,16 @@ const NoteMap = () => {
     const handleMouseUp = () => {
         setIsDragging(false);
     };
+
+    const scaleLimits = { min: 0.1, max: 4 };
+
     const handleWheel = (e: React.WheelEvent) => {
         e.preventDefault();
         const delta = e.deltaY * -0.001;
-        const newScale = Math.min(Math.max(0.1, scale + delta), 4);
+        const newScale = Math.min(
+            Math.max(scaleLimits.min, scale + delta),
+            scaleLimits.max
+        );
         setScale(newScale);
     };
 
@@ -105,9 +157,6 @@ const NoteMap = () => {
                 </Tooltip>
             </TooltipProvider>
             <DialogContent className="min-w-full min-h-full p-0 rounded-none sm:rounded-none ">
-                <div className="fixed bottom-4 right-4 z-50">
-                    <SettingsDialog />
-                </div>
                 <DialogHeader className="sr-only">
                     <DialogTitle className="sr-only">
                         Are you absolutely sure?
@@ -118,6 +167,35 @@ const NoteMap = () => {
                         servers.
                     </DialogDescription>
                 </DialogHeader>
+                <div className="fixed  z-50 bottom-0 left-0 w-full px-3 backdrop-blur-md p-2 flex gap-4 border-t border-x justify-between rounded-t-[var(--radius)] flex-wrap ">
+                    <div className="w-fit mx-auto">
+                        <Toolbar exclude={{ noteMap: true }} />
+                    </div>
+                    <div className="flex justify-between items-center gap-2 mx-auto">
+                        <Controls
+                            scale={scale}
+                            setScale={setScale}
+                            scaleLimits={scaleLimits}
+                        />
+                        <div className="flex gap-2">
+                            <Button
+                                size={"sm"}
+                                onClick={() => setPosition({ x: 0, y: 0 })}
+                                className="flex items-center gap-2"
+                            >
+                                <Undo2 className="size-5" /> Position
+                            </Button>
+                            <Button
+                                size={"sm"}
+                                onClick={() => setScale(1)}
+                                className="flex items-center gap-2"
+                            >
+                                <Undo2 className="size-5" /> Zoom
+                            </Button>
+                        </div>
+                    </div>
+                </div>
+
                 <div className="w-full h-full  relative select-none ">
                     <div
                         ref={containerRef}
