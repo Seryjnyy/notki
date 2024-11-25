65b8c6993100f26a2455183b3992f8fa3f078a9e
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-25 14:00:02 +0000

    Integrate vaults & recents lists to sidebar

--- apps/app/src/components/sidebar/app-sidebar.tsx
+++ apps/app/src/components/sidebar/app-sidebar.tsx
@@ -1,25 +1,18 @@
-import { Button } from "@repo/ui/components/ui/button";
 import {
     Sidebar,
     SidebarContent,
     SidebarFooter,
-    SidebarGroup,
-    SidebarGroupContent,
-    SidebarGroupLabel,
     SidebarHeader,
-    SidebarMenu,
-    SidebarMenuButton,
-    SidebarMenuItem,
     SidebarRail,
 } from "@repo/ui/components/ui/sidebar";
-import { Clock, EllipsisVertical, Vault } from "lucide-react";
+import { Clock, Vault } from "lucide-react";
 import * as React from "react";
-import { Vault as VaultType } from "~/lib/backend-types";
-import { Recent, useGetSortedRecents } from "~/stores/recents-store";
-// import { useSidebarViewStore } from "~/stores/sidebar-view-store";
 import { SearchCurrentViewForm } from "./search-form";
 import { CurrentViewSwitcher } from "./view-switcher";
+
 import { ManageVaultsDialogTrigger } from "../vault-manager/manage-vaults-dialog";
+import SidebarGroupRecents from "./app-sidebar-group-recents";
+import SidebarGroupVaults from "./app-sidebar-group-vaults";
 import AppSidebarShortcut from "./app-sidebar-shortcut";
 import { OpenFolderDialogTrigger } from "./open-folder-dialog";
 
@@ -75,63 +68,19 @@ const SidebarCurrentViewProvider = ({
     );
 };
 
-const SidebarGroupRecents = () => {
-    const { search } = useSidebarCurrentView();
-    const sortedRecents = useGetSortedRecents();
-
-    const filteredRecents = React.useMemo(() => {
-        return sortedRecents.filter((item) => {
-            return item.path.toLowerCase().includes(search.toLowerCase());
-        });
-    }, [sortedRecents, search]);
-
-    return (
-        <SidebarGroup>
-            <SidebarGroupLabel className="flex justify-between">
-                <span className={search != "" ? "text-primary" : ""}>
-                    Recents
-                </span>
-                <span>{filteredRecents.length}</span>
-            </SidebarGroupLabel>
-            <SidebarGroupContent className="space-y-4">
-                <SidebarMenu>
-                    <RecentsList recents={filteredRecents} />
-                </SidebarMenu>
-            </SidebarGroupContent>
-        </SidebarGroup>
-    );
-};
-
-const SidebarGroupVaults = () => {
-    const { search } = useSidebarCurrentView();
-    const { vaults } = useVaults();
-
-    const filteredVaults = React.useMemo(() => {
-        return vaults.filter((item) => {
-            return item.filepath.toLowerCase().includes(search.toLowerCase());
-        });
-    }, [vaults, search]);
-
-    return (
-        <SidebarGroup>
-            <SidebarGroupLabel className="flex justify-between">
-                <span className={search != "" ? "text-primary" : ""}>
-                    Vaults
-                </span>
-                <span>{filteredVaults.length}</span>
-            </SidebarGroupLabel>
-            <SidebarGroupContent className="space-y-4">
-                <SidebarMenu>
-                    <VaultsList vaults={filteredVaults} />
-                </SidebarMenu>
-            </SidebarGroupContent>
-        </SidebarGroup>
-    );
+const CurrentView = () => {
+    const { currentView } = useSidebarCurrentView();
+    switch (currentView) {
+        case "recents":
+            return <SidebarGroupRecents />;
+        case "vaults":
+            return <SidebarGroupVaults />;
+        default:
+            return null;
+    }
 };
 
 export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
-    const { currentView } = useSidebarCurrentView();
-
     return (
         <SidebarCurrentViewProvider>
             <Sidebar {...props}>
@@ -141,8 +90,7 @@ export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
                 </SidebarHeader>
 
                 <SidebarContent>
-                    {currentView === "Recents" && <SidebarGroupRecents />}
-                    {currentView === "Vaults" && <SidebarGroupVaults />}
+                    <CurrentView />
                 </SidebarContent>
 
                 <SidebarFooter>
