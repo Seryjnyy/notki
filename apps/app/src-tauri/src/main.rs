14ce6156f7c093ddf402634c63755d7300cfa7df
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-28 20:47:33 +0000

    Fix linting issue in main.rs
    - Fixed import for Command, it didn't trigger an error for the other
    calls to this since it wasn't getting to them, this is because it was
    developed on Windows.
    
    - Added return to show_in_explorer to fix issue with ignoring potential
    error.

--- apps/app/src-tauri/src/main.rs
+++ apps/app/src-tauri/src/main.rs
@@ -8,6 +8,8 @@ use typeshare::typeshare;
 
 use nanoid::nanoid;
 
+use std::process::Command;
+
 use std::io::{BufWriter, Write};
 use std::path::{Path, PathBuf};
 
@@ -25,14 +27,14 @@ lazy_static! {
 // TODO : untested on other platforms
 // But works for now
 #[tauri::command]
-async fn show_in_explorer(path: String) {
+async fn show_in_explorer(path: String) -> Result<(), String> {
     // TODO : No error handling whatsoever
     #[cfg(target_os = "windows")]
     {
-        let _ = std::process::Command::new("explorer")
+        Command::new("explorer")
             .args(["/select,", &path])
             .spawn()
-            .map_err(|e| e.to_string());
+            .map_err(|e| e.to_string())?;
     }
 
     #[cfg(target_os = "macos")]
@@ -72,6 +74,8 @@ async fn show_in_explorer(path: String) {
             }
         }
     }
+
+    Ok(())
 }
 
 use std::fs;
