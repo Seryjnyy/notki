0049f9f3919c438704f854dd054403907de9f29a
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-11-28 23:15:40 +0000

    Fix eslint no-extra-semi in shortcut-aware-dialog
    - Fixed in shortcut-aware-dialog-trigger.tsx.

--- packages/ui/src/components/shortcut/shortcut-aware-dialog-trigger.tsx
+++ packages/ui/src/components/shortcut/shortcut-aware-dialog-trigger.tsx
@@ -9,7 +9,7 @@ interface ShortcutAwareDialogTriggerProps
   shortcut?: Shortcut
 }
 
-// Utility to merge refs, incase it is needed
+// Utility to merge refs, in case it is needed
 // TODO : not tested fully
 function mergeRefs<T>(...refs: (React.Ref<T> | null | undefined)[]) {
   return (value: T) => {
@@ -17,6 +17,8 @@ function mergeRefs<T>(...refs: (React.Ref<T> | null | undefined)[]) {
       if (typeof ref === "function") {
         ref(value)
       } else if (ref != null) {
+        // Prettier keeps adding a semicolon here and eslint is complaining so ignore it
+        // eslint-disable-next-line no-extra-semi
         ;(ref as React.MutableRefObject<T>).current = value
       }
     })
