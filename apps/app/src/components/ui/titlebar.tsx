b7e1c97e1ca0ade8bba8eb6873a14b11fa263dad
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-04 23:41:23 +0000

    Update title-bar based on fullscreen
    - In fullscreen the title-bar should behave differently. It will now
    hide the window buttons, but they remain there for the user to use by
    focusing or hovering on them.
    
    - When in fullscreen it now also returns a slightly different title-bar
    that doesn't have data-tauri-drag-region, so the user doesn't drag the
    window about.

--- apps/app/src/components/ui/titlebar.tsx
+++ apps/app/src/components/ui/titlebar.tsx
@@ -1,6 +1,9 @@
 import { BoxIcon, Cross1Icon, MinusIcon } from "@radix-ui/react-icons"
 import { appWindow } from "@tauri-apps/api/window"
 import { ReactNode } from "react"
+import { useFullscreen } from "~/hooks/use-fullscreen"
+import { cn } from "@repo/ui/lib/utils"
+import { Button } from "@repo/ui/components/ui/button"
 
 interface WindowActionButtonProps {
   children: ReactNode
@@ -8,44 +11,61 @@ interface WindowActionButtonProps {
 }
 const WindowActionButton = ({ children, action }: WindowActionButtonProps) => {
   return (
-    <div
-      className="inline-flex justify-center items-center h-[30px] w-[30px] hover:bg-primary group"
+    <Button
+      className="inline-flex justify-center items-center h-[30px] w-[30px] hover:bg-primary group rounded-none"
       onClick={action}
+      variant={"ghost"}
     >
       <span className="text-primary group-hover:text-secondary">
         {children}
       </span>
-    </div>
+    </Button>
+  )
+}
+
+const WindowActions = () => {
+  return (
+    <>
+      <WindowActionButton action={() => appWindow.minimize()}>
+        <MinusIcon className="" />
+      </WindowActionButton>
+
+      <WindowActionButton action={() => appWindow.toggleMaximize()}>
+        <BoxIcon />
+      </WindowActionButton>
+
+      <WindowActionButton action={() => appWindow.close()}>
+        <Cross1Icon />
+      </WindowActionButton>
+    </>
   )
 }
 
 export default function Titlebar() {
+  const { isFullscreen } = useFullscreen()
+
+  if (isFullscreen)
+    return (
+      <div className="select-none h-[30px] flex justify-between w-full z-[200] brightness-125">
+        <div
+          className={cn(
+            "ml-auto",
+            isFullscreen &&
+              "opacity-0  focus-within:opacity-100 hover:opacity-100"
+          )}
+        >
+          <WindowActions />
+        </div>
+      </div>
+    )
+
   return (
     <div
       data-tauri-drag-region
-      className="select-none h-[30px] flex justify-between   w-full z-[200] brightness-125"
+      className="select-none h-[30px] flex justify-between   w-full z-[200] brightness-125 "
     >
-      {/*<div>*/}
-      {/*  <div className="flex items-center justify-center flex-col text-primary  h-full text-xs">*/}
-      {/*    <span>Workspace</span>*/}
-      {/*    /!* <span>*/}
-      {/*                  C:/Users/Documents/example/path/thenSome/thenThere/Thenhere*/}
-      {/*              </span> *!/*/}
-      {/*  </div>*/}
-      {/*</div>*/}
-
-      <div className="ml-auto">
-        <WindowActionButton action={() => appWindow.minimize()}>
-          <MinusIcon className="" />
-        </WindowActionButton>
-
-        <WindowActionButton action={() => appWindow.toggleMaximize()}>
-          <BoxIcon />
-        </WindowActionButton>
-
-        <WindowActionButton action={() => appWindow.close()}>
-          <Cross1Icon />
-        </WindowActionButton>
+      <div className={"ml-auto"}>
+        <WindowActions />
       </div>
     </div>
   )
