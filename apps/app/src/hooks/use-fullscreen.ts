5f6c058ff40043d2f3d4d76558a3e89f89cf93d5
Jakub Wojcik <jakubwojcik6a@gmail.com>
2024-12-04 23:01:20 +0000

    Add listener to check if fullscreen triggered
    - Added listener on window resize in use-fullscreen.ts, this is to
    ensure that the isFullscreen state is kept upto date. This is important
    because if title bar is hidden in fullscreen and user leaves fullscreen
    in some way then the app won't know and title bar hidden when it's
    needed.
    
    - Added lodash for debounce.

--- apps/app/src/hooks/use-fullscreen.ts
+++ apps/app/src/hooks/use-fullscreen.ts
@@ -1,5 +1,7 @@
-import {useEffect, useState} from "react";
+import {useCallback, useEffect, useState} from "react";
 import {appWindow} from "@tauri-apps/api/window";
+import {debounce} from "lodash";
+import {UnlistenFn} from "@tauri-apps/api/helpers/event";
 
 export const useFullscreen = () => {
     const [isFullscreen, setIsFullscreen] = useState<boolean | undefined>(
@@ -16,8 +18,52 @@ export const useFullscreen = () => {
     const toggleFullscreen = async () => {
         await appWindow.setFullscreen(!(await appWindow.isFullscreen()))
         // Fetch updated value in case state mismatch
+        // Might not be needed because the listener will detect screen size change and update state
         setIsFullscreen(await appWindow.isFullscreen())
     }
+
+     const updateFullscreen = useCallback(async()=> {
+        setIsFullscreen(await appWindow.isFullscreen())
+    }, [])
+
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    const debouncedUpdate = useCallback(debounce(updateFullscreen, 300), [updateFullscreen]);
+
+    useEffect(() => {
+        // Track component mounting state
+        let isMounted = true;
+
+        const listenToResize = async () => {
+            const cleanup = await appWindow.onResized(() => {
+                debouncedUpdate()
+            })
+
+            // Returns clean up logic
+            // Call cleanup if component is still mounted
+            return () => {
+                if(isMounted) {
+                    cleanup()
+                }
+            }
+        }
+
+        // Set up listener
+        let cleanupPromise : UnlistenFn;
+        listenToResize().then(cleanupFunc => {
+            cleanupPromise = cleanupFunc
+        })
+
+        return () => {
+            // Prevent state updates if component unmounts
+            isMounted = false
+
+            // Call clean up if listening function started listening and returned a cleanup func
+            if(cleanupPromise) {
+                cleanupPromise()
+            }
+        }
+    }, [debouncedUpdate]);
+
 
     return {
         isFullscreen,
