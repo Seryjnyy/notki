6183c36efd92e2e008637381606554366328a393
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-02-02 00:08:01 +0000

    Fix x and y overflow in note slideshow
    - Overflow in slideshow mode caused a bottom scrollbar to appear which
    in turn would trigger the side scrollbar to appear too. This then
    overlapped the close window action button, making it un-clickable.
    
    - Fixed this by displaying a single note using the same elements and
    style as in the grid display.

--- packages/ui/src/components/display-notes/single-note.tsx
+++ packages/ui/src/components/display-notes/single-note.tsx
@@ -1,56 +1,61 @@
-import { Circle } from "lucide-react";
-import NoteCard from "@repo/ui/components/display-notes/note-card";
+import NoteCard from "@repo/ui/components/display-notes/note-card"
 
-import { useCallback } from "react";
-import { useNoteList } from "@repo/ui/providers/note-list-provider";
-import { useNoteDisplaySettings } from "@repo/lib/stores/note-display-settings-store";
+import { useCallback, useMemo } from "react"
+import { useNoteList } from "@repo/ui/providers/note-list-provider"
+import { useNoteDisplaySettings } from "@repo/lib/stores/note-display-settings-store"
 
 export default function SingleNote({
-    removeNote,
+  removeNote,
 }: {
-    removeNote: (noteID: string) => void;
+  removeNote: (noteID: string) => void
 }) {
-    const paddingX = useNoteDisplaySettings.use.paddingX();
-    const paddingY = useNoteDisplaySettings.use.paddingY();
-    const { notes, activeIndex, setActiveIndex } = useNoteList();
+  const paddingX = useNoteDisplaySettings.use.paddingX()
+  const paddingY = useNoteDisplaySettings.use.paddingY()
+  const { notes, activeIndex, setActiveIndex } = useNoteList()
 
-    const handleRemoveNote = useCallback(
-        (noteID: string) => {
-            removeNote(noteID);
+  const handleRemoveNote = useCallback(
+    (noteID: string) => {
+      removeNote(noteID)
 
-            if (activeIndex >= notes.length - 1) {
-                setActiveIndex(activeIndex - 1);
-            }
-        },
-        [activeIndex, notes, removeNote]
-    );
+      if (activeIndex >= notes.length - 1) {
+        setActiveIndex(activeIndex - 1)
+      }
+    },
+    [activeIndex, notes, removeNote]
+  )
 
-    const getNote = useCallback(() => {
-        if (notes.length == 0 || activeIndex < 0 || activeIndex >= notes.length)
-            return null;
+  const getNote = useCallback(() => {
+    if (notes.length == 0 || activeIndex < 0 || activeIndex >= notes.length)
+      return null
 
-        const note = notes[activeIndex];
+    const note = notes[activeIndex]
 
-        if (!note) return null;
+    if (!note) return null
 
-        return (
-            <NoteCard note={note} key={note.id} onDelete={handleRemoveNote} />
-        );
-    }, [notes, activeIndex]);
+    return note
+  }, [notes, activeIndex])
 
-    return (
-        <div
-            className="mb-16"
-            style={{
-                padding: `${paddingY}px ${paddingX}px`,
-            }}
-        >
-            <div className="relative">
-                {getNote()}
-                <div className="absolute bottom-2  right-2">
-                    <Circle className="size-3 text-primary" />
-                </div>
-            </div>
-        </div>
-    );
+  const currentNote = useMemo(() => getNote(), [getNote])
+
+  // Basically the same code as with grid display
+  return (
+    <ul
+      className="grid"
+      style={{
+        gridTemplateColumns: `repeat(1, minmax(0, 1fr))`,
+        gap: `0px`,
+        padding: `${paddingY}px ${paddingX}px`,
+      }}
+    >
+      {currentNote && (
+        <li key={currentNote.id} className={`relative`}>
+          <NoteCard note={currentNote} onDelete={handleRemoveNote} />
+          <div className="absolute bottom-2  right-2">
+            <div className="size-3 border-2 border-primary rounded-[var(--radius)]"></div>
+            {/*  <Circle className="size-3 text-primary" />*/}
+          </div>
+        </li>
+      )}
+    </ul>
+  )
 }
