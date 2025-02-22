f10804efe24dba4b6050bcaa11d326c89861ce3b
Jakub Wojcik <jakubwojcik6a@gmail.com>
2025-02-22 22:13:09 +0000

    Add feedback when reading in folders
    - Changed use-upload-notes-from-dirs.ts to .tsx.
    
    - Changed how folders are processed to get info on which one failed or
    succeeded.
    
    - Changed function to return the succeeded and failed paths.
    
    - Added toast to show how many notes were read in from the successfully
    processed folders.

--- apps/app/src/hooks/use-upload-notes-from-dirs.tsx
+++ apps/app/src/hooks/use-upload-notes-from-dirs.tsx
@@ -2,11 +2,14 @@ import useUploadNotes from "@repo/lib/hooks/use-upload-notes"
 import { guidGenerator } from "@repo/lib/utils/metadata-utils"
 import { getTxtFilesWithAllDetail } from "~/lib/services/directory-service.ts"
 import { Note } from "@repo/lib/types/types"
+import { useToast } from "@repo/ui/hooks/use-toast"
 
 // Returns the paths that were successfully uploaded
 // TODO : no error handling whatsoever ;-;
 export const useUploadNotesFromDirs = () => {
   const uploadNotes = useUploadNotes()
+  const { toast } = useToast()
+
   return async ({
     dirs,
     recursive = false,
@@ -17,9 +20,21 @@ export const useUploadNotesFromDirs = () => {
     replace?: boolean
   }) => {
     const paths = Array.isArray(dirs) ? dirs : [dirs]
+    const succeededPaths: string[] = []
+    const failedPaths: string[] = []
 
     const txtFiles = await Promise.all(
-      paths.map((path) => getTxtFilesWithAllDetail(path, recursive))
+      paths.map(async (path) => {
+        try {
+          const res = await getTxtFilesWithAllDetail(path, recursive)
+          succeededPaths.push(path)
+          return res
+        } catch (error) {
+          console.error(error)
+          failedPaths.push(path)
+          return []
+        }
+      })
     ).then((res) => res.flat())
 
     const readInNotes: Note[] = txtFiles.map((file) => ({
@@ -33,6 +48,12 @@ export const useUploadNotesFromDirs = () => {
     }))
 
     uploadNotes(readInNotes, replace)
-    return paths
+
+    toast({
+      title: "Reading in notes",
+      description: `Read in ${readInNotes.length} notes from ${succeededPaths.length}/${paths.length} folders.`,
+    })
+
+    return { success: succeededPaths, fail: failedPaths }
   }
 }
